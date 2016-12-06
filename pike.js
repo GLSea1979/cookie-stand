3dc92a11d084013898c284d44745920366c29d60
Gary Lundgren <builtbygl@gmail.com>
2016-12-05 16:47:35 -0800

    second level of object, it works sorta

--- pike.js
+++ pike.js
@@ -1,28 +1,3 @@
-var alkiStore = {
-  name: 'Alki Store',
-  minCustHour:  2,
-  maxCustHour:  16,
-  avgCookiesCust: 4.6,
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', 'noon', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],   //written in military time for future maths
-  cookiesSoldPerHour : [],
-  randomCustHour: function() {
-    for (var i = 0; i < 15; i++) {
-      var hourlyCustomers = Math.floor(Math.random() * alkiStore.maxCustHour + alkiStore.minCustHour);
-      var cookieRandomizer = Math.round((hourlyCustomers * alkiStore.avgCookiesCust) * 100 / 100);
-      // console.log(hourlyCustomers + 'customers per hour.');
-      // console.log(cookieRandomizer + 'cookies per hour.');
-      this.cookiesSoldPerHour.push(cookieRandomizer);
-    }
-  }
-}
-alkiStore.randomCustHour();
-var listList = document.getElementById('list');
-for (var i= 0; i < 14; i++) {
-  var liEl = document.createElement('li');
-  liEl.textContent = alkiStore.hoursOpen[i] + ': ' +  alkiStore.cookiesSoldPerHour[i] + ' cookies';
-  listList.appendChild(liEl);
-}
-
 var pikeStore = {
   name: 'Alki Store',
   minCustHour:  2,
