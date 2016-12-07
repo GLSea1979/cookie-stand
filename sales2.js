0753d6a9301b7a5915c8046007a98cfa0133e09b
Gary Lundgren <builtbygl@gmail.com>
2016-12-07 13:05:38 -0800

    starting events, html set up

--- sales2.js
+++ sales2.js
@@ -3,7 +3,13 @@ var universal =[];
 var testingTotal = [];
 var finalTotal = [];
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', 'noon', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
-
+// make objects
+new Store('Alki', 2, 16, 4.6);
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('1st and Pike', 23, 65, 6.3);
+// Store constuctor +++++++++++++++++++++++++++++++++++++
 function Store(storeName, minCustHour, maxCustHour, avgCookiesCust) {
   this.storeName = storeName;
   this.minCustHour = minCustHour;
@@ -46,54 +52,51 @@ function Store(storeName, minCustHour, maxCustHour, avgCookiesCust) {
   testingTotal.push(this.cookiesSoldPerHour);
   universal.push(this);
 }
-// make objects
-new Store('Alki', 2, 16, 4.6);
-new Store('SeaTac Airport', 3, 24, 1.2);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('1st and Pike', 23, 65, 6.3);
-//make table header
-
-
-
-
+//make table header +++++++++++++++++++++++++++++
 function tablebuilder() {
   var storeTable = document.getElementById('storeTable');
   var trEl= document.createElement('tr');
   var blankEL = document.createElement('th');
   storeTable.appendChild(blankEL);
   for (var i = 0; i < hoursOpen.length; i++) {
-    var thEl= document.createElement('th');
+     var thEl= document.createElement('th');
     thEl.textContent = hoursOpen[i];
     trEl.appendChild(thEl);
     storeTable.appendChild(thEl);
   }
 }
-tablebuilder();
-for (var i = 0; i < universal.length; i++) {
-  universal[i].render();
-}
-var totalTotal = 0;
-var totalEl = document.getElementById('storeTable');
-var trEl = document.createElement('tr');
-var tdEl = document.createElement('td');
-tdEl.textContent = 'Total';
-totalEl.appendChild(trEl);
-totalEl.appendChild(tdEl);
-
-for (var i = 0; i < (hoursOpen.length - 1); i++) {
-  for (var j = 0; j < testingTotal.length; j++) {
-    totalTotal += testingTotal[j][i];
+//  Stores in an Array  are rendered+++++++++++++++++++++++++++++++++
+function putObjectsIntoArray() {
+  for (var i = 0; i < universal.length; i++) {
+    universal[i].render();
   }
-  var tdEl = document.createElement('td');
-  tdEl.textContent = totalTotal;
+}
+// Build the er Footer with totals+++++++++++++++++++++++++++++++++++
+function buildsTableTotalsRow(){
+  var totalTotal = 0;
+  var totalEl = document.getElementById('storeTable');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('th');
+  tdEl.textContent = 'Total';
+  totalEl.appendChild(trEl);
   totalEl.appendChild(tdEl);
-  totalTotal = 0;
+  for (var i = 0; i < (hoursOpen.length - 1); i++) {
+    for (var j = 0; j < testingTotal.length; j++) {
+      totalTotal += testingTotal[j][i];
+    }
+    var tdEl = document.createElement('th');
+    tdEl.textContent = totalTotal;
+    totalEl.appendChild(tdEl);
+    totalTotal = 0;
+  }
+  var lastTotal = 0;
+  for (var i = 0; i < finalTotal.length; i++){
+    lastTotal += finalTotal[i];
+  }
+  var tdEl1 = document.createElement('th')
+  tdEl1.textContent = lastTotal;
+  totalEl.appendChild(tdEl1);
 }
-var lastTotal = 0;
-for (var i = 0; i < finalTotal.length; i++){
-  lastTotal += finalTotal[i];
-}
-var tdEl1 = document.createElement('td')
-tdEl1.textContent = lastTotal;
-totalEl.appendChild(tdEl1);
+tablebuilder();
+putObjectsIntoArray();
+buildsTableTotalsRow();
